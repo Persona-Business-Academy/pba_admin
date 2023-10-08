f1944f2d85757387feb5920d42f025776a2f566a
Hayk Kyutneryan <77750836+kyutneryan@users.noreply.github.com>
2023-10-08 14:36:29 +0400

    fix

--- src/lib/aws/index.ts
+++ src/lib/aws/index.ts
@@ -2,9 +2,9 @@ import AWS from "aws-sdk";
 
 export const s3 = new AWS.S3({
   credentials: {
-    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY,
-    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
   },
-  region: process.env.NEXT_PUBLIC_AWS_REGION,
+  region: process.env.AWS_REGION,
   useAccelerateEndpoint: true,
 });
