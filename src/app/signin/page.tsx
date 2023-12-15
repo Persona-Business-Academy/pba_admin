3abe7230024f9f76c8b23d59ce0092522a8d39ec
Hayk Kyutneryan <77750836+kyutneryan@users.noreply.github.com>
2023-12-15 12:35:25 +0400

    added enter event

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import { Box, Button, Flex, useToast } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
@@ -10,127 +10,132 @@ import { DASHBOARD_ROUTE } from "@/constants/routes";
 import { SignInFormData } from "@/models/auth";
 
 function Signin() {
- const toast = useToast();
- const { push } = useRouter();
+  const toast = useToast();
+  const { push } = useRouter();
 
- const {
-  control,
-  handleSubmit,
-  formState: { errors, isSubmitting },
- } = useForm<SignInFormData>({
-  defaultValues: { email: "", password: "" },
- });
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignInFormData>({
+    defaultValues: { email: "", password: "" },
+  });
 
- const onSubmit: SubmitHandler<SignInFormData> = useCallback(
-  ({ email, password }) => {
-   signIn("credentials", {
-    email,
-    password,
-    redirect: false,
-   })
-    .then((res) => {
-     if (res?.ok) {
-      push(DASHBOARD_ROUTE);
-     } else {
-      toast({
-       title: "Invalid credentials",
-       description: ERROR_MESSAGES.invalidCredentials,
-       status: "error",
-      });
-     }
-    })
-    .catch(() => {
-     toast({
-      title: ERROR_MESSAGES.somethingWentWrong,
-      status: "error",
-     });
-    });
-  },
-  [push, toast]
- );
+  const onSubmit: SubmitHandler<SignInFormData> = useCallback(
+    ({ email, password }) => {
+      signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      })
+        .then(res => {
+          if (res?.ok) {
+            push(DASHBOARD_ROUTE);
+          } else {
+            toast({
+              title: "Invalid credentials",
+              description: ERROR_MESSAGES.invalidCredentials,
+              status: "error",
+            });
+          }
+        })
+        .catch(() => {
+          toast({
+            title: ERROR_MESSAGES.somethingWentWrong,
+            status: "error",
+          });
+        });
+    },
+    [push, toast],
+  );
 
- return (
-  <Flex
-   direction="column"
-   alignSelf="center"
-   justifySelf="center"
-   overflow="hidden"
-  >
-   <Box
-    minH={{ base: "70vh", md: "100vh" }}
-    borderRadius={{ md: "15px" }}
-    bg="#dedede"
-   >
-    <Flex alignItems="center" justifyContent="center" mb="60px" mt="20px">
-     <Flex
-      direction="column"
-      w="445px"
-      background="transparent"
-      borderRadius="15px"
-      p="40px"
-      mx={{ base: "100px" }}
-      bg="#fff"
-      boxShadow="0 20px 27px 0 rgb(0 0 0 / 5%)"
-     >
-      <Controller
-       name="email"
-       control={control}
-       rules={{
-        required: "This field is required",
-       }}
-       render={({ field: { onChange, value } }) => (
-        <FormInput
-         isRequired
-         isInvalid={!!errors.email?.message}
-         name="email"
-         type="email"
-         formLabelName="Email"
-         value={value}
-         handleInputChange={onChange}
-         formErrorMessage={errors.email?.message}
-        />
-       )}
-      />
+  const handleKeyPress = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.key === "Enter") {
+        handleSubmit(onSubmit)();
+      }
+    },
+    [handleSubmit, onSubmit],
+  );
 
-      <Controller
-       name="password"
-       control={control}
-       rules={{
-        required: "This field is required",
-       }}
-       render={({ field: { onChange, value } }) => (
-        <FormInput
-         isRequired
-         isInvalid={!!errors.password?.message}
-         name="password"
-         formLabelName="Password"
-         value={value}
-         handleInputChange={onChange}
-         type="password"
-         formHelperText="Your password must be less than 6 characters."
-         formErrorMessage={errors.password?.message}
-        />
-       )}
-      />
-      <Button
-       type="submit"
-       bg="#3CB4E7"
-       fontSize="sm"
-       color="white"
-       fontWeight="bold"
-       w="100%"
-       h="45"
-       my="24px"
-       isDisabled={isSubmitting}
-       onClick={handleSubmit(onSubmit)}
-      >
-       SIGN IN
-      </Button>
-     </Flex>
+  useEffect(() => {
+    document.addEventListener("keypress", handleKeyPress);
+    return () => {
+      document.removeEventListener("keypress", handleKeyPress);
+    };
+  }, [handleKeyPress]);
+
+  return (
+    <Flex direction="column" alignSelf="center" justifySelf="center" overflow="hidden">
+      <Box minH={{ base: "70vh", md: "100vh" }} borderRadius={{ md: "15px" }} bg="#dedede">
+        <Flex alignItems="center" justifyContent="center" mb="60px" mt="20px">
+          <Flex
+            direction="column"
+            w="445px"
+            background="transparent"
+            borderRadius="15px"
+            p="40px"
+            mx={{ base: "100px" }}
+            bg="#fff"
+            boxShadow="0 20px 27px 0 rgb(0 0 0 / 5%)">
+            <Controller
+              name="email"
+              control={control}
+              rules={{
+                required: "This field is required",
+              }}
+              render={({ field: { onChange, value } }) => (
+                <FormInput
+                  isRequired
+                  isInvalid={!!errors.email?.message}
+                  name="email"
+                  type="email"
+                  formLabelName="Email"
+                  value={value}
+                  handleInputChange={onChange}
+                  formErrorMessage={errors.email?.message}
+                />
+              )}
+            />
+
+            <Controller
+              name="password"
+              control={control}
+              rules={{
+                required: "This field is required",
+              }}
+              render={({ field: { onChange, value } }) => (
+                <FormInput
+                  isRequired
+                  isInvalid={!!errors.password?.message}
+                  name="password"
+                  formLabelName="Password"
+                  value={value}
+                  handleInputChange={onChange}
+                  type="password"
+                  formHelperText="Your password must be less than 6 characters."
+                  formErrorMessage={errors.password?.message}
+                />
+              )}
+            />
+            <Button
+              type="submit"
+              bg="#3CB4E7"
+              fontSize="sm"
+              color="white"
+              fontWeight="bold"
+              w="100%"
+              h="45"
+              my="24px"
+              isDisabled={isSubmitting}
+              onClick={handleSubmit(onSubmit)}>
+              SIGN IN
+            </Button>
+          </Flex>
+        </Flex>
+      </Box>
     </Flex>
-   </Box>
-  </Flex>
- );
+  );
 }
 
 export default Signin;
